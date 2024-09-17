0bf497007bc71921bf16d3013cbe281622995e14
romerito <you@example.com>
2024-09-17 02:43:07 +0100

    ops. vi que essa linha não era mais necessária. sorry

--- tests/testes_da_calculadora_de_cambio.py
+++ tests/testes_da_calculadora_de_cambio.py
@@ -15,8 +15,7 @@ class TestesDaCalculadoraDeCambio(TestCase):
     def teste_casa_decimal(self):
         valor = 2
         taxa = 4.334
-        resultado_esperado = 8.67
-
+        
         resultado_obtido = calc.converter(valor, taxa)
         
         tem_duas_casas_decimais = (resultado_obtido * 100) % 1 == 0
