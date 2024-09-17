6381a4cb935f44e8d0e10bd928d5ceae2bc494c2
romerito <you@example.com>
2024-09-17 13:41:51 +0100

    removendo o teste de exemplo

--- tests/testes_da_calculadora_de_cambio.py
+++ tests/testes_da_calculadora_de_cambio.py
@@ -3,15 +3,6 @@ import src.calculadora_de_cambio as calc
 
 class TestesDaCalculadoraDeCambio(TestCase):
 
-    def teste_exemplo(self):
-        valor = 1
-        taxa = 2
-        resultado_esperado = 2
-
-        resultado_obtido = calc.converter(valor, taxa)
-
-        self.assertEqual(resultado_obtido, resultado_esperado)
-
     def teste_casa_decimal(self):
         valor = 2
         taxa = 4.334
